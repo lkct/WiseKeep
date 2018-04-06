8ab5aba21529cca4b8ebff557a640e79ae721888
lkct <liangkct@yahoo.com>
2018-04-06 13:54:04 +0800

    modify drawer menu

--- app/src/main/java/javaprog/wisekeep/WiseKeep.java
+++ app/src/main/java/javaprog/wisekeep/WiseKeep.java
@@ -80,17 +80,13 @@ public class WiseKeep extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_expense) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_income) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_stats) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
